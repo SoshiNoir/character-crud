47846a37409d1251f8d0653924e174741c194373
Miguel Matias <migbonamim@gmail.com>
2025-05-01 17:34:43 -0300

    Add ProfilePage component for user profile display

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const ProfilePage = () => {
+  return (
+    <div className='container mx-auto p-4'>
+      <h1 className='text-3xl font-bold mb-6'>Perfil</h1>
+      <p>Bem-vindo ao seu perfil!</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
